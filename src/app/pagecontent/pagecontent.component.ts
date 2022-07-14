b00a153bc558b0a282f9752653fd0448db72e45e
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2022-07-14 10:13:06 +0500

    make some changes in pagecomponent

--- src/app/pagecontent/pagecontent.component.ts
+++ src/app/pagecontent/pagecontent.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentService } from "../content.service";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagecontent',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagecontent.component.scss']
 })
 export class PagecontentComponent implements OnInit {
+  res: any;
+  terms: any;
+  cont: any;
 
-  constructor() { }
+  constructor(private contentservice:ContentService, private router: Router) { }
 
   ngOnInit(): void {
+    this.Getcontent()
   }
+  Getcontent()
+  {
+    this.contentservice.Getcontent().subscribe((data:any)=>{
+      this.res=data;
+      this.terms= this.res[1].pageContentTitle;
+      this.cont= this.res[1].Content;
+      console.log(this.res);
+    })
+  }
+  GetcontentById(id:number)
+  {
+    this.router.navigate(['/Details'], { queryParams: { Id: id } });
+  }  
 
 }
