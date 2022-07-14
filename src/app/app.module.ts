3df7328e8dbd5727455a3ca8f316db7101400e26
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2022-07-14 10:03:37 +0500

    reconfiguring app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CKEditorModule } from 'ng2-ckeditor';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TexteditorComponent } from './texteditor/texteditor.component';
 import { PagecontentComponent } from './pagecontent/pagecontent.component';
@@ -17,7 +19,10 @@ import { DetailspostComponent } from './detailspost/detailspost.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CKEditorModule
+    CKEditorModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
