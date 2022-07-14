26e1072274d8a5affa48ebb00b110446a36decb8
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2022-07-14 09:58:20 +0500

    Adding Content service

---
+++ src/app/content.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContentService {
+  url :any;
+  constructor(private http: HttpClient) { }
+
+  // AddUpdateContent(pagecontent) {
+  //     debugger
+  //     this.url = 'http://localhost:56357/Api/Contents/saveconetnt';
+  //     return this.http.post(this.url, pagecontent);
+  // }
+  // Getcontent() {
+  //   debugger
+  //   this.url = 'http://localhost:56357/Api/Contents/Getpagdata';
+  //   return this.http.get(this.url);
+  // }
+  // GetcontentById(Id) {
+  //   debugger
+  //   this.url = 'http://localhost:56357/Api/Contents/GetpagecontentBy?Id='+Id;
+  //   return this.http.get(this.url);
+  // }
+}
