a4c1d87caaec942601021112c490f98a7c46544d
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2022-07-14 10:18:54 +0500

    configuring routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagecontentComponent } from './pagecontent/pagecontent.component';
+import { TexteditorComponent } from './texteditor/texteditor.component';
+import { DetailspostComponent } from './detailspost/detailspost.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TexteditorComponent },
+  { path: 'Post', component: PagecontentComponent },
+  { path: 'Details', component: DetailspostComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
