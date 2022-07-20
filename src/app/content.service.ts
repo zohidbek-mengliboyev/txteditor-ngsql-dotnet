a85a721cc80acffca0e4cf00aec4a7ba3c7ca7d1
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2022-07-20 11:28:41 +0500

    removing comments

--- src/app/content.service.ts
+++ src/app/content.service.ts
@@ -32,29 +32,4 @@ export class ContentService {
     return this.http.get<Content>(this.txtEditorUrl + `/PostContent/Get/${id}`);
   }
 
-  // AddUpdateContent(pagecontent: any) {
-  //     // debugger
-  //     // if (pagecontent == null) {
-  //     this.myUrl = 'http://localhost:7212/api/PostContent/Create';
-  //     return this.http.post(this.myUrl, pagecontent);
-  //     // }
-  //     // else {
-  //     //   this.myUrl = 'http://localhost:46673/api/PostContent/Update/id'+pagecontent;
-  //     //   return this.http.post(this.myUrl, pagecontent);
-  //     // }
-  //     // this.url = 'http://localhost:56357/Api/Contents/saveconetnt';
-  //     // return this.http.post(this.url, pagecontent);
-  // }
-  // Getcontent() {
-  //   // debugger
-  //   // this.url = 'http://localhost:56357/Api/Contents/Getpagdata';
-  //   this.myUrl = 'http://localhost:7212/api/PostContent/GetAll';
-  //   return this.http.get(this.myUrl);
-  // }
-  // GetcontentById(Id: number) {
-  //   // debugger
-  //   // this.url = 'http://localhost:56357/Api/Contents/GetpagecontentBy?Id='+Id;
-  //   this.myUrl = 'http://localhost:7212/api/PostContent/Get/id='+Id;
-  //   return this.http.get(this.myUrl);
-  // }
 }
